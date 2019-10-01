2ee0146db14987d60dcbff45018bd254e15b5e39
zhaohuiwei <azhaohuiwei@163.com>
2019-10-01 17:33:21 +0800

    字符串加减乘除 需要的类

---
+++ src/demos/DivideResult.java
@@ -0,0 +1,32 @@
+package demos;
+
+/**
+ * Created by huiwei.zhao on 2019/7/25.
+ */
+public class DivideResult {
+    /**
+     * 取整结果
+     */
+    private String r;
+
+    /**
+     * 取余结果
+     */
+    private String yu;
+
+    public String getR() {
+        return r;
+    }
+
+    public void setR(String r) {
+        this.r = r;
+    }
+
+    public String getYu() {
+        return yu;
+    }
+
+    public void setYu(String yu) {
+        this.yu = yu;
+    }
+}
