ce530544e4312b20f45643520229ab70fe44fda8
zhaohuiwei <azhaohuiwei@163.com>
2019-10-01 17:35:51 +0800

    链表节点定义

---
+++ src/demos/LinkNode.java
@@ -0,0 +1,16 @@
+package demos;
+
+/**
+ * Created by huiwei.zhao on 2019/6/24.
+ */
+public class LinkNode {
+    public int val;
+    public LinkNode next;
+
+    public LinkNode() {
+    }
+
+    public LinkNode(int x) {
+        this.val = x;
+    }
+}
