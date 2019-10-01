7613106de01a6e480c1f01590e825f4dff88ea6e
zhaohuiwei <azhaohuiwei@163.com>
2019-10-01 17:39:16 +0800

    绝对路径和相对路径

--- src/demos/PathDemo.java
+++ src/demos/PathDemo.java
@@ -3,6 +3,7 @@ package demos;
 import java.io.File;
 
 public class PathDemo {
+    
     public static void main(String[] args) {
         File file = new File("/");
         System.out.println("/代表的绝对路径为：" + file.getAbsolutePath());
